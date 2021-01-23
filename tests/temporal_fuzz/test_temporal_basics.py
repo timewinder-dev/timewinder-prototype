0e1eb196a8f9e4d071d0e6f81d990c4304d79945
Barak Michener <me@barakmich.com>
2021-01-23 14:29:15 -0800

    make ttraces a thing and compare them to the bool equivalents

--- tests/temporal_fuzz/test_temporal_basics.py
+++ tests/temporal_fuzz/test_temporal_basics.py
@@ -3,77 +3,102 @@ from hypothesis.strategies import lists, booleans
 
 from typing import List
 
+TTrace = List[bool]
 
-def always(l: List[bool]) -> bool:
+
+def to_bool(l: TTrace) -> bool:
+    return l[0]
+
+
+def always_bool(l: TTrace) -> bool:
     return all(l)
 
 
-def eventually(l: List[bool]) -> bool:
+def always(l: TTrace) -> TTrace:
+    if len(l) == 0:
+        return []
+    return [all(l)] + always(l[1:])
+
+
+def eventually_bool(l: TTrace) -> bool:
     return any(l)
 
 
-def eventuallyAlways(l: List[bool]) -> bool:
+def eventually(l: TTrace) -> TTrace:
+    if len(l) == 0:
+        return []
+    return [any(l)] + eventually(l[1:])
+
+
+def eventuallyAlways_bool(l: TTrace) -> bool:
     if len(l) == 0:
         return True
     if len(l) == 1:
         return l[0]
     if l[0] is True:
-        return always(l[1:]) or eventuallyAlways(l[1:])
+        return always_bool(l[1:]) or eventuallyAlways_bool(l[1:])
     else:
-        return eventuallyAlways(l[1:])
+        return eventuallyAlways_bool(l[1:])
 
 
-def alwaysEventually(l: List[bool]) -> bool:
+def alwaysEventually_bool(l: TTrace) -> bool:
     if len(l) == 0:
         return False
     if len(l) == 1:
         return l[0]
     if l[0] is True:
-        return alwaysEventually(l[1:])
+        return alwaysEventually_bool(l[1:])
     else:
-        return eventually(l[1:]) and alwaysEventually(l[1:])
+        return eventually_bool(l[1:]) and alwaysEventually_bool(l[1:])
 
 
-def inverse(l: List[bool]) -> List[bool]:
+def inverse(l: TTrace) -> TTrace:
     return [not x for x in l]
 
 
-def leadsTo(p, q: List[bool]) -> bool:
+def leadsTo_bool(p, q: TTrace) -> bool:
     assert len(p) == len(q)
     if len(p) == 0:
         return True
     if p[0] is True:
         if q[0] is True:
-            return leadsTo(p[1:], q[1:])
+            return leadsTo_bool(p[1:], q[1:])
         else:
-            return eventually(q[1:]) and leadsTo(p[1:], q[1:])
+            return eventually_bool(q[1:]) and leadsTo_bool(p[1:], q[1:])
     else:
-        return leadsTo(p[1:], q[1:])
+        return leadsTo_bool(p[1:], q[1:])
+
+
+def leadsTo(p, q: TTrace) -> TTrace:
+    pass
 
 
 @given(lists(booleans()))
 def test_always(l):
     assume(len(l) > 0)
-    if always(l):
-        assert eventuallyAlways(l)
+    assert to_bool(always(l)) == always_bool(l)
+    if always_bool(l):
+        assert eventuallyAlways_bool(l)
+        assert to_bool(eventually(always(l))) == always_bool(l)
 
 
 @given(lists(booleans()))
 def test_eventually(l):
     assume(len(l) > 0)
-    if alwaysEventually(l):
-        assert eventually(l)
+    assert to_bool(eventually(l)) == eventually_bool(l)
+    assert to_bool(always(eventually(l))) == alwaysEventually_bool(l)
+    if alwaysEventually_bool(l):
+        assert eventually_bool(l)
 
 
 @given(lists(booleans()))
 @settings(max_examples=2000)
 def test_eventually_not_always_not(p):
-    assume(len(p) > 0)
-    assert eventually(p) == (not always(inverse(p)))
+    assert eventually(p) == inverse(always(inverse(p)))
 
 
 @given(lists(booleans()))
 @settings(max_examples=2000)
 def test_leads_to_equivalence(p):
     assume(len(p) > 0)
-    assert leadsTo(inverse(p), p) == alwaysEventually(p)
+    assert leadsTo_bool(inverse(p), p) == to_bool(always(eventually(p)))
