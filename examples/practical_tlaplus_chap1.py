ef4d74d611ab374f923d6c3aad18a31655574957
Barak Michener <me@barakmich.com>
2021-03-19 21:02:57 -0700

    Add example

---
+++ examples/practical_tlaplus_chap1.py
@@ -0,0 +1,55 @@
+import timewinder
+from timewinder.generators import Set
+
+
+@timewinder.model
+class Account:
+    def __init__(self, name, amt):
+        self.name = name
+        self.acc = amt
+
+
+@timewinder.process
+def check_and_withdraw(sender, reciever, amt):
+    if amt <= sender.acc:
+        sender.acc = sender.acc - amt
+        yield "deposit"
+        reciever.acc = reciever.acc + amt
+
+
+@timewinder.process
+def withdraw(sender, reciever, amount):
+    sender.acc = sender.acc - amount
+    yield "deposit"
+    reciever.acc = reciever.acc + amount
+
+
+alice = Account("alice", 5)
+bob = Account("bob", 5)
+
+
+no_overdrafts = timewinder.ForAll(Account, lambda a: a.acc >= 0)
+
+
+ev = timewinder.Evaluator(
+    models=[alice, bob],
+    threads=[withdraw(alice, bob, 6)],
+    # threads=[
+        # check_and_withdraw(alice, bob, Set(range(1, 7))),
+        # check_and_withdraw(alice, bob, Set(range(1, 7))),
+    # ],
+    specs=[no_overdrafts],
+)
+err = None
+try:
+    ev.evaluate()
+except timewinder.ConstraintError as e:
+    err = e
+
+if err is None:
+    print(ev.stats)
+else:
+    print("\n" + err.name + "\n")
+    ev.replay_thunk(err.thunk)
+
+
